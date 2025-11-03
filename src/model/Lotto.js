8880b07fb9e994b603f18ed59034adc9efd4607b
IM_SORRY <imwe25@naver.com>
2025-11-03 16:04:29 +0900

    feat: model 구현

---
+++ src/model/Lotto.js
@@ -0,0 +1,31 @@
+export default class Lotto {
+  #numbers;
+
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = [...numbers].sort((a,b) => a-b);
+  }
+
+  #validate(numbers) {
+    if (!Array.isArray(numbers)){
+      throw new Error("[ERROR] 로또 번호는 배열이어야 합니다.")
+    }
+    if (numbers.length !== 6) {
+      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+    const set = new Set(numbers);
+    if (set.size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.")
+    }
+    const inRangeInt = numbers.every( n =>
+      Number.isInteger(n) && n >= 1 && n <= 45
+    );
+    if (!inRangeInt){
+      throw new Error("[ERROR] 로또 번호는 정수이면서 1~45 사이의 숫자여야 합니다.")
+    }
+  }
+
+  get numbers() {
+    return [...this.#numbers];
+  }
+}
---
+++ src/model/WinningNumber.js
@@ -0,0 +1,27 @@
+import Lotto from './Lotto.js';
+
+export default class WinningNumber {
+    #set;
+    #bouns;
+
+    constructor(mainNumbers, bouns){
+        const lotto = new Lotto(mainNumbers);
+        this.#set = new Set(lotto.numbers);
+
+        if(!Number.isInteger(bouns) || bouns < 1 || bouns > 45){
+            throw new Error('[ERROR] 보너스 번호는 1~45사이의 정수여야 합니다.');
+        }
+        if (this.#set.has(bouns)){
+            throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
+        }
+        this.#bouns = bouns;
+    }
+
+    has(n) {
+        return this.#set.has(n);
+    }
+
+    isBonusMatched(ticket){
+        return ticket.numbers.includes(this.#bouns);
+    }
+}
